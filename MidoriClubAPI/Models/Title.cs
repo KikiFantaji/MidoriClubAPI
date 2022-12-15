d74c74ef5887e24bc09151ba76dc827050c55f9e
ii <ii@ii.ii>
2022-12-15 12:55:13 +0300

    Добавьте файлы проекта.

---
+++ MidoriClubAPI/Models/Title.cs
@@ -0,0 +1,23 @@
+﻿namespace MidoriClubAPI.Models
+{
+    public class Title
+    {
+        public int Id { get; set; }
+
+        public string Headline { get; set; }
+
+        public string AlternativeHeadline { get; set; }
+
+        public DateOnly DateCreated { get; set; }
+
+        public string Image { get; set; }
+
+        public string Type { get; set; }
+
+        public string Episodes { get; set; }
+
+        public string EpisodeDuration { get; set; }
+
+        public string Rating { get; set; }
+    }
+}
